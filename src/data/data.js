403c07817629b4ae0847ad792d803c47e30b4a3d
이은주 <dldmswn071918@gmail.com>
2023-11-08 18:06:31 +0900

    프로젝트 데이터 수정

--- src/data/data.js
+++ src/data/data.js
@@ -22,17 +22,6 @@ export const ProjectList = [
       github : "https://github.com/dldmswn0719/lucky"
     },
     {
-      img: './../Images/puriproject.svg',
-      title: 'Puripuri',
-      type: '팀 프로젝트',
-      desc: '협업을 하여 만든 유기동물 사이트입니다. 스토어페이지를 담당하였습니다. ',
-      duration: '2023.08.21 ~ 10.09',
-      skills: 'react, firebase, redux, tailwindcss',
-      contribution: '30%',
-      web : "https://team-puri.vercel.app/",
-      github : "https://github.com/dldmswn0719/Team-Puri"
-    },
-    {
       img: './../Images/mcdonald_redesign.svg',
       title: 'Mcdonald_Redesign',
       type: '개인 프로젝트',
@@ -66,6 +55,17 @@ export const ProjectList = [
       github : "https://github.com/dldmswn0719/parcel"
     },
     {
+      img: './../Images/puriproject.svg',
+      title: 'Puripuri',
+      type: '팀 프로젝트',
+      desc: '협업을 하여 만든 유기동물 사이트입니다. 스토어페이지를 담당하였습니다. ',
+      duration: '2023.08.21 ~ 10.09',
+      skills: 'react, firebase, redux, tailwindcss',
+      contribution: '30%',
+      web : "https://team-puri.vercel.app/",
+      github : "https://github.com/dldmswn0719/Team-Puri"
+    },
+    {
       img: './../Images/quiz.svg',
       title: 'Quiz',
       type: '개인 프로젝트',
@@ -77,25 +77,25 @@ export const ProjectList = [
       github : "https://github.com/dldmswn0719/quiz"
     },
     {
-      img: './../Images/mcdonald.svg',
-      title: 'Mcdonald',
-      type: '클론코딩',
-      desc: '기존 맥도날드 사이트를 클론코딩한 사이트입니다.',
-      duration: '',
-      skills: 'react, tailwindcss ',
-      contribution: '100%',
-      web : "https://mc-donald-clone.vercel.app/",
-      github : "https://github.com/dldmswn0719/McDonald_clone"
-    },
-    {
       img: './../Images/subway.svg',
       title: 'Subway',
       type: '클론코딩',
       desc: '기존 서브웨이 사이트를 클론코딩한 사이트입니다.',
-      duration: '',
-      skills: 'react, styled-components',
+      duration: '2023.07.20 ~',
+      skills: 'html,css,javascript',
       contribution: '100%',
-      web : "https://react-subway.vercel.app/",
-      github : "https://github.com/dldmswn0719/React_subway"
+      web : "https://subway-red.vercel.app/",
+      github : "https://github.com/dldmswn0719/subway"
+    },
+    {
+      img: './../Images/mcdonald.svg',
+      title: 'Mcdonald',
+      type: '클론코딩',
+      desc: '기존 맥도날드 사이트를 클론코딩한 사이트입니다.',
+      duration: '2023.07.25 ~ 08.16',
+      skills: 'html,css,javascript',
+      contribution: '100%',
+      web : "https://mcdonald-eight.vercel.app/",
+      github : "https://github.com/dldmswn0719/mcdonald"
     }
   ]
