b40e3c60ffedfd6ab1d711a205abab6e91912b65
이은주 <dldmswn071918@gmail.com>
2023-11-28 15:55:09 +0900

    board desc 수정

--- src/data/projectList.js
+++ src/data/projectList.js
@@ -10,7 +10,7 @@ export const ProjectList = [
         "en": "It is the first portfolio site of a new developer, Eunju Lee, created to show his personal competence and experience.",
         "kr": "개인적인 역량과 경험을 직접 보여주기 위해 제작한 이은주 신입 개발자의 첫 포트폴리오 사이트입니다."
       },
-      duration: '2023.10.19 ~ ',
+      duration: '2023.10.19 ~ 2023.11.14',
       skills: 'react, tailwindcss , redux',
       contribution: '100%',
       web : "https://portfolio-hazel-beta-32.vercel.app/",
@@ -24,11 +24,11 @@ export const ProjectList = [
         "kr": "개인"
       },
       desc: {
-        "en": "This is a bulletin board created using MySQL. Comments, membership registration, and login functions are implemented, and an additional administrator dashboard is also being developed.",
-        "kr": "MySQL을 활용하여 만든 게시판입니다. 댓글, 회원가입, 로그인 기능이 구현되어 있고 또한 관리자 대시보드도 추가로 개발중입니다."
+        "en": "This is a bulletin board created using MySQL. Additional administrator dashboards are also being developed.",
+        "kr": "MySQL을 활용하여 만든 게시판입니다. 관리자 대시보드도 추가로 개발중입니다."
       },
       duration: '2023.11.06 ~ ',
-      skills: 'next.js, typescript, MySQL, tailwindcss',
+      skills: 'next.js, typescript, MySQL',
       contribution: '100%',
       web : "https://mysql-board-ruby.vercel.app/",
       github : "https://github.com/dldmswn0719/mysql_board"
